6d96d581f55887fe4b4fd762e9fc3caf19b88930
fadykittan <fady.kittan@gmail.com>
2020-05-23 21:22:33 +0300

    adding some code

--- question_1/src/GenericQueue.java
+++ question_1/src/GenericQueue.java
@@ -1,34 +1,45 @@
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 
-public class GenericQueue <T> {
+public class GenericQueue<T> {
 
-    private PriorityQueue <QueueElement<T>> queue;
+    private PriorityQueue<QueueElement<T>> queue;
     private static final int MIN = 1;
     private static final int MAX = 10;
     private int priorityNum;
 
-    public GenericQueue (int priorityNum){
-        if(priorityNum <= MAX && priorityNum >= MIN) {
+    public GenericQueue(int priorityNum) {
+        if (priorityNum <= MAX && priorityNum >= MIN) {
             this.priorityNum = priorityNum;
+        } else {
+            priorityNum = MAX;
         }
-        else { priorityNum = MAX;}
+        this.priorityNum = priorityNum;
         queue = new PriorityQueue<>(new ElementComparator());
     }
 
-    public void add (T data , int p ){
-        if (p>=priorityNum && p<=MIN){
-            p=priorityNum;
+    public void add(T data, int p) {
+        if (p >= priorityNum && p <= MIN) {
+            p = priorityNum;
         }
-        QueueElement<T> element = new QueueElement<T>(data , p);
+        QueueElement<T> element = new QueueElement<>(data, p);
         queue.add(element);
     }
 
 
+    public T poll() {
+        QueueElement<T> element = queue.poll();
+        if (element == null) {
+            return null;
+        } else {
+            return element.get_element();
+        }
+    }
 
-
-
-
+    public boolean contains(T toFind) {
+        QueueElement<T> e = new QueueElement<>(toFind);
+        return queue.contains(e);
+    }
 
 
 
--- question_1/src/QueueElement.java
+++ question_1/src/QueueElement.java
@@ -1,18 +1,27 @@
-public class QueueElement <T> {
+public class QueueElement<T> {
 
     private T _element;
     private int _priority;
 
-    public QueueElement (T element, int priority){
+    public QueueElement(T element, int priority) {
         this._element = element;
         this._priority = priority;
     }
 
-    public T get_element(){
+    public QueueElement(T element) {
+        this._element = element;
+    }
+
+    public T get_element() {
         return this._element;
     }
 
-    public int get_priority(){
+    public int get_priority() {
         return this._priority;
+    }
+
+
+    public boolean equals(QueueElement<T> other) {
+        return this._element.equals(other._element);
     }
 }
