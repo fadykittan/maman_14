649d20fc274c308132d2cf8aa6aa082cdf80572a
fadykittan <fady.kittan@gmail.com>
2020-06-07 23:00:07 +0300

    FE

---
+++ question_2/src/CalendarView.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class CalendarView extends JPanel {
+
+    private final int DAYS_IN_ROW = 7;
+    private final int DAYS_IN_COLUMN = 6;
+    private final int MARGIN = 0;
+
+    public CalendarView() {
+        setLayout(new GridLayout(DAYS_IN_ROW, DAYS_IN_COLUMN + 1));
+
+        add(new JLabel("Sunday", SwingConstants.CENTER));
+        add(new JLabel("Monday", SwingConstants.CENTER));
+        add(new JLabel("Tuesday", SwingConstants.CENTER));
+        add(new JLabel("Wednesday", SwingConstants.CENTER));
+        add(new JLabel("Thursday", SwingConstants.CENTER));
+        add(new JLabel("Friday", SwingConstants.CENTER));
+        add(new JLabel("Saturday", SwingConstants.CENTER));
+
+        for (int i = 1; i <= DAYS_IN_COLUMN * DAYS_IN_ROW; i++) {
+            add(new DayView(new ArrayList<>()));
+        }
+    }
+}
---
+++ question_2/src/EventsView.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class EventsView extends JPanel {
+
+    public void paintComponent(Graphics g) {
+        g.setColor(Color.lightGray);
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
+
+}
